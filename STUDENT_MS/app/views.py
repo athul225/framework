5b573805911c04529ac05542b5d4c546c0b7a3b0
athul225 <athulviju599@gmail.com>
2025-02-28 16:13:14 +0530

    newdocument

--- STUDENT_MS/app/views.py
+++ STUDENT_MS/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 
 # Create your views here.
@@ -12,5 +12,15 @@ def index(request):
         dname=Department.objects.get(pk=dpk)
         data=Students.objects.create(name=name,age=age,email=email,dname=dname)
         data.save()
-
+        return redirect(seeall)
     return render(request,'index.html',{'dnames':dnames})
+
+def seeall(request):
+    dnames=Department.objects.all()
+    stds=Students.objects.all()
+    if request.method=='POST':
+        dpk=request.POST['dname']
+        dname=Department.objects.get(pk=dpk)
+        stds=Students.objects.filter(dname=dname)
+
+    return render(request,'seeall.html',{'dnames':dnames,'stds':stds})
