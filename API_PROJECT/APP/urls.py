04fab28d44a96cb5e95cdfe9f026839ffc692b1d
athul225 <athulviju599@gmail.com>
2025-03-15 17:12:03 +0530

    newdocument

--- API_PROJECT/APP/urls.py
+++ API_PROJECT/APP/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns=[
     path('',views.fun),
     path('fun2',views.fun2),
-    path('fun3/<d>',views.fun3)
+    path('fun3/<d>',views.fun3),
+    path('fun4',views.fun4),
+    path('fun5/<d>',views.fun5),
+    path('fun6',views.fun6.as_view()),
+    path('fun7/<d>',views.fun7.as_view()),
+    path('fun8',views.genericapiview.as_view()),
+    path('fun9/<id>',views.update.as_view())
+
 ]
--- API_PROJECT/APP/views.py
+++ API_PROJECT/APP/views.py
@@ -4,6 +4,11 @@ from . models import *
 from . serializers import *
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics,mixins
 
 
 # Create your views here.
@@ -55,3 +60,102 @@ def fun3(request,d):
     elif request.method=='DELETE':
         demo.delete()
         return JsonResponse("Deleted")
+    
+@api_view(['GET','POST'])
+def fun4(request):
+    
+    if request.method=='GET':
+        d=Student.objects.all()
+        s=model_serializers(d,many=True)
+        return Response(s.data)
+
+    elif request.method=='POST':
+        s=model_serializers(data=request.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(s.errors,status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET','POST','PUT','DELETE'])
+def fun5(request,d):
+    
+    try:
+        demo=Student.objects.get(pk=d)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET':
+        s=model_serializers(demo)
+        return Response(s.data)
+    elif request.method=='PUT':
+        s=model_serializers(demo,data=request.data)
+        if s.is_valid():
+            s.save()
+            return Response(s.data)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method=='DELETE':
+        demo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class fun6(APIView):
+    def get(self,request):
+        demo=Student.objects.all()
+        s=model_serializers(demo,many=True)
+        return Response(s.data)
+    def post(self,request):
+        s=model_serializers(data=request.data)
+        if s.is_valid():
+            s.save()
+            return Response(s.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+class fun7(APIView):
+    def get(self,request,d):
+        try:
+            demo=Student.objects.get(pk=d)
+            s=model_serializers(demo)
+            return Response(s.data)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def put(self,request,d):
+        try:
+            demo=Student.objects.get(pk=d)
+            s=model_serializers(demo,data=request.data)
+            if s.is_valid():
+                s.save()
+                return Response(s.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def delete(self,req,d):
+        
+        try:
+            demo=Student.objects.get(pk=d)
+            demo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+class genericapiview(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin):
+    serializer_class=model_serializers
+    queryset=Student.objects.all()
+    def get(self,request):
+        return self.list(request)
+    def post(self,request):
+        return self.create(request)
+
+
+class update(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+    serializer_class=model_serializers
+    queryset=Student.objects.all()
+    lookup_field='id'
+    def get(self,request):
+        return self.list(request)
+    def put(self,request,id):
+        return self.create(request,id)
+    def delete(self,request,id):
+        return self.create(request,id)
+    
